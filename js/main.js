b3c7be8406af63124ecbbb242b564e10a6e3e9a0
Devkrishna1 <devkrishnavijayaradhan@gmail.com>
2024-05-12 21:18:39 +0100

    Initial Responsive design implementation

--- js/main.js
+++ js/main.js
@@ -1,41 +1,46 @@
 $(document).ready(function() {
-    $('.flexslider').flexslider({
-      animation: "slide",
-      animationLoop: false,
-      slideshow: false,
-      itemWidth: 840,
-      itemMargin: 5,
-      minItems: 1,
-      maxItems: 3, 
-      controlNav: false,
-      directionNav: true,
-      prevText: "",
-      nextText: "",
-      move: 3  
-    });
-  });
+  // General settings for all sliders
+  function getFlexSliderSettings(min, max, move, itemWidth) {
+      return {
+          animation: "slide",
+          animationLoop: false,
+          slideshow: false,
+          itemWidth: itemWidth,
+          itemMargin: 5,
+          minItems: min,
+          maxItems: max,
+          controlNav: false,
+          directionNav: true,
+          prevText: "",
+          nextText: "",
+          move: move
+      };
+  }
 
-  $(document).ready(function() {
-    $('.flexslider.skills').flexslider({
-      animation: "slide",
-      animationLoop: false,
-      itemWidth: 688,
-      itemMargin: 5,
-      minItems: 1,
-      maxItems: 3, 
-      controlNav: false,
-      directionNav: true,
-      prevText: "", 
-      nextText: "",
-      move: 3  
-    });
-  });
+  // Initializing Project FlexSlider
+  $('.flexslider').flexslider(getFlexSliderSettings(1, 3, 1, 840));
 
+  // Initializing Skills FlexSlider
+  $('.flexslider.skills').flexslider(getFlexSliderSettings(1, 3, 1, 688));
+
+  // Adjusting sliders on window resize for more responsiveness
+  $(window).resize(function() {
+      var windowWidth = $(window).width();
+      var settings_project = (windowWidth < 768) ? getFlexSliderSettings(1, 1, 1, 210) : getFlexSliderSettings(1, 3, 1, 840);
+      var settings_skills = (windowWidth < 768) ? getFlexSliderSettings(1, 1, 1, 210) : getFlexSliderSettings(1, 3, 1, 688);
+      $('.flexslider').data('flexslider').vars = $.extend($('.flexslider').data('flexslider').vars, settings_project);
+      $('.flexslider.skills').data('flexslider').vars = $.extend($('.flexslider.skills').data('flexslider').vars, settings_skills);
+  });
+});
+
+// Smooth scrolling for all anchors
+$(document).ready(function() {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
 });
